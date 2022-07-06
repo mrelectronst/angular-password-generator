9f72c3f929740525d1585f0c667f9a9bf4472cbe
Fatih Karaman <fatih.karaman.dn@aygaz.com.tr>
2022-07-06 14:43:18 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,62 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'passgenerator';
+
+  password:string = '';
+  passLength: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+
+  modifyLength(value: string) {
+    console.log(value);
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.passLength = parsedValue;
+    }
+  }
+
+  modifyLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  modifyNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  modifySymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  buttonClick() {
+    const numbers = '0123456789';
+    const letters = 'qwertyuopasdfghjklizxcvbnm';
+    const symbols = '!%&/()]}|[{^++%&';
+
+    let validChars = '';
+
+    if(this.includeLetters){
+      validChars += letters;
+    }
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    let passGenerated = '';
+
+    for(let i= 0; i<this.passLength; i++){
+      const index = Math.floor(Math.random()*validChars.length);
+      passGenerated += validChars[index];
+    }
+
+    this.password = passGenerated;
+  }
 }
